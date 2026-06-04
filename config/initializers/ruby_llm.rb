edae345f003b7838f30379ba0b428ab55b17eda1
Oleksii Zaprii <ozaprii@Oleksiis-MBP.lan>
2026-06-04 15:06:45 -0700

    Ollama

---
+++ config/initializers/ruby_llm.rb
@@ -0,0 +1,8 @@
+RubyLLM.configure do |config|
+  # config.openai_api_key = ENV.fetch("OPENAI_API_KEY", Rails.application.credentials.dig(:openai_api_key))
+  # config.default_model = "gpt-5-nano"
+  config.ollama_api_base = "http://localhost:11434/v1"
+
+  # Use the new association-based acts_as API (recommended)
+  config.use_new_acts_as = true
+end
